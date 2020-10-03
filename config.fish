35cf5c0b0ad6d1629818d18f8ec02f2ad17f4a20
Sam Purkis <sam.purkis@hotmail.co.uk>
2020-10-04 00:23:52 +0100

    [add files] add initial files

---
+++ config.fish
@@ -0,0 +1,9 @@
+
+source /home/sam/envs/py8/bin/activate.fish
+
+alias fishbs="nano ~/.config/fish/config.fish"
+alias t="tmuxinator start ts"
+
+alias bs="nano ~/.bashrc"
+
+alias envs="cd ~/envs"
---
+++ terminal_setup_install.sh
@@ -0,0 +1,1 @@
+sudo apt install fish tmux tilda tmuxinator
