b8935e290889284c1260a315e15eb6c2803552af
Sam Purkis <sam.purkis@hotmail.co.uk>
2020-10-04 00:32:36 +0100

    [install script] corrected install script

--- terminal_setup_install.sh
+++ terminal_setup_install.sh
@@ -1,1 +1,13 @@
+chmod 644 $PWD/tilda.service 
+
 sudo apt install fish tmux tilda tmuxinator
+
+cat $PWD/bashrc_addition >> ~/.bashrc
+
+mkdir --parents ~/.config/fish/; mv $PWD/config.fish $_
+mkdir --parents ~/.config/tmuxinator/; mv $PWD/ts.yml $_
+mkdir --parents ~/.config/systemd/user/; mv $PWD/tilda.service $_
+
+systemctl --user enable tilda.service
+systemctl --user daemon-reload
+echo "Reboot to have settings take effect"
