d3b18f59e4ca9210c03c5a7c73233c2afb201202
Sam Purkis <sam.purkis@hotmail.co.uk>
2022-08-11 16:54:24 +0100

    remove unreachable apt source

--- terminal_setup_install.sh
+++ terminal_setup_install.sh
@@ -3,9 +3,6 @@
 # install set of requirements
 sudo add-apt-repository ppa:linuxuprising/guake -y
 sudo add-apt-repository ppa:apt-fast/stable -y
-echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 sudo apt update
 sudo apt install nala -y
 sudo nala install fish tmux guake -y
